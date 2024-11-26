0206ce9f239d6f5ec4cff3e90c338580029f3c9a
Cpooh <cpooh2001@gmail.com>
2024-11-26 13:00:29 +0530

    About Section Changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Services from '../section/service/Services'
+import AboutUs from '../section/about/AboutUs'
 
 const Home = () => {
   return (
     <div className='flex flex-col bg-white'>
+      <AboutUs/>
       <Services/>
     </div>
   )
