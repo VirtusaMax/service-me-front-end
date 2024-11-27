17ec75264d483068f50c832ececcf9b1fc7bf799
IndunilRanganath <indunilranganath071@gmail.com>
2024-11-27 23:41:03 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from 'react';
 import Home from './pages/Home';
-
 import Navbar from './pages/Navbar';
 
 function App() {
   return (
-    <div className='flex justify-center'>
-      <Home/>
-    <div className="App">
+    <div className="flex flex-col justify-center">
       <Navbar />
-      <h1 className="text-3xl font-bold text-center my-8">Welcome to Service Me!</h1>
+      <div className="App">
+        <h1 className="text-3xl font-bold text-center my-8">Welcome to Service Me!</h1>
+        <Home />
+      </div>
     </div>
   );
 }
 
-export default App
 export default App;
