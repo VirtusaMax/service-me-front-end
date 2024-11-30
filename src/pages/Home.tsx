50608edd79cf8247bedb17443e37ead4c099ba72
J.A.C.P Jayakodi <164337545+CpoohZchala@users.noreply.github.com>
2024-11-30 14:46:49 +0530

    Revert "Feat/about section"

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Services from '../section/service/Services'
-import AboutUs from '../section/about/AboutUs'
-import Navbar from '../components/custom-components/Navbar'
+import ServiceSection from '../sections/home/ServiceSection'
 
 const Home = () => {
   return (
-    <div className='flex flex-col bg-white max-w-[1600px]'>
-      <Navbar/>
-      <AboutUs/>
-      <Services/>
+    <div>
+     <ServiceSection/>
     </div>
   )
 }
