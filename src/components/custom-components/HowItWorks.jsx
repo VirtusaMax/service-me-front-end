4fcd518c2171bc5e6cc3cc4342324b50c805bf48
D.T.Thennakoon <154307023+dinithiZ@users.noreply.github.com>
2024-12-09 18:35:34 +0530

    update worksection

--- src/components/custom-components/HowItWorks.jsx
+++ src/components/custom-components/HowItWorks.jsx
@@ -1,25 +1,24 @@
 import React from "react";
-import backgroundImage from "../../assets/work_section/worksbg.png"; // Adjust path as per your project structure
+import backgroundImage from "../../assets/work_section/worksbg.png"; 
 
 const HowItWorks = () => {
   return (
     <div
-      className="relative bg-cover bg-center py-20 px-14 h-[600px] w-full"
+      className="relative bg-cover bg-center py-20 px-14 h-[600px] w-full top-4"
       style={{
         backgroundImage: `url(${backgroundImage})`,
       }}
     >
-      {/* Overlay for Better Contrast */}
-      <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+      
+      <div className="absolute inset-0 bg-white bg-opacity-85"></div>
 
-      {/* Content */}
+      
       <div className="relative z-10 w-full max-w-4xl">
-        {/* Title Section */}
-        <h2 className="text-yellow-400 text-3xl font-extrabold mb-8 text-left drop-shadow-lg">
+         
+        <h2 className="text-yellow-400 text-3xl font-extrabold mb-8 text-left">
           HOW IT WORKS?
         </h2>
-
-        {/* Video Placeholder */}
+ 
         <div className="bg-gray-300 flex justify-center items-center py-20 px-6 md:py-32 md:h-[400px]">
           <p className="text-xl font-bold text-indigo-900">
             NEED TO ADD VIDEO FOR THIS SPACE
