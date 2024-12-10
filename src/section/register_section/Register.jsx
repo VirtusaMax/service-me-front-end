e9508afbda9d5689efdf40c0427c3d8dae366c2e
Cpooh <cpooh2001@gmail.com>
2024-12-10 21:52:24 +0530

    updated code

--- src/section/register_section/Register.jsx
+++ src/section/register_section/Register.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
-
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-
 import background from '../../assets/register_section/backlogo.png';
 import profileData from "../../data/profileData.json";
 
