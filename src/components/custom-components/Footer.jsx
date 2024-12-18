1110602c5ef0908cadc9f744f5e2118557db6519
D.T.Thennakoon <154307023+dinithiZ@users.noreply.github.com>
2024-12-18 20:02:31 +0530

    update footer

--- src/components/custom-components/Footer.jsx
+++ src/components/custom-components/Footer.jsx
@@ -12,23 +12,8 @@ const Footer = () => {
         backgroundPosition: 'center',
       }}
     >
-       
-      <div className="custom-shape-divider-top">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 800 90"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            className="relative block fill-white"
-          ></path>
-        </svg>
-      </div>
-
-    
-      <div className="max-w-screen-2xl mx-auto px-8 md:px-16">
+      
+      <div className="max-w-screen-2xl mx-auto px-8 md:px-16 mt-6">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
            
           <div>
@@ -62,11 +47,7 @@ const Footer = () => {
                   About Us
                 </a>
               </li>
-              <li>
-                <a href="/contact" className="hover:text-yellow-300">
-                  Contact Us
-                </a>
-              </li>
+              
             </ul>
           </div>
 
@@ -136,3 +117,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
