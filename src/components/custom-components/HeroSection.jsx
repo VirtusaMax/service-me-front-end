c6303ff493aff172b43ab0d917270a75aebebb46
D.T.Thennakoon <154307023+dinithiZ@users.noreply.github.com>
2024-12-25 10:59:22 +0530

    update serviceProvider

--- src/components/custom-components/HeroSection.jsx
+++ src/components/custom-components/HeroSection.jsx
@@ -1,34 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import heroImg from '../../assets/hero_section/heropic.png'
+import heroImg from '../../assets/hero_section/heropic.png';
 
 const HeroSection = () => {
-    return(
-        <div className='relative top-20 bg-[#0E0524] flex flex-col md:flex-row h-[800px] md:h-[500px] justify-center items-center'>
-            <div className='w-full md:w-1/2 flex flex-col justify-center py-20 md:pr-16 xl:pr-40 md:py-0 border px-6 md:px-0'>
+    return (
+       // <div className='top-20 bg-[#0E0524] grid grid-cols-1 md:grid-cols-2 min-h-[650px] '>
+       <div className=' top-20 bg-[#0E0524] flex flex-col md:flex-row h-[800px] md:h-[650px] justify-center items-center'>
+            {/* Left Section */}
+            <div className='flex flex-col justify-center py-20 px-6 md:pr-16 xl:pr-40 md:py-0'>
                 <div className='text-center text-white md:text-left space-y-4'>
-                    <p className='text-5xl font-semibold lg:text-7xl'> Your Trusted Partner for home services </p>
-                   
-                    <p className='flex-wrap'>New way to find people to do your jobs. If you are one of those thousands of people who struggle to find people to do Jobs or struggle to find jobs, this is for you.</p>
-                  
-                  <div className="space-x-2">
-                    <button className="bg-[#FCC215] text-[#0E0524] font-bold text-s h-[39px] px-6 py-0 rounded-full mt-2">
-                    I want to work
-                    </button>
-                    <button className="bg-[#FCC215] text-[#0E0524] font-bold text-s h-[39px] px-6 py-0 rounded-full mt-2">
-                    I want to hire</button> 
+                    <p className='text-3xl md:text-5xl font-semibold lg:text-6xl'>
+                        Your Trusted Partner for Home Services
+                    </p>
+                    <p className='flex-wrap'>
+                        New way to find people to do your jobs. If you are one of those thousands of people who struggle to find people to do jobs or struggle to find jobs, this is for you.
+                    </p>
+                    <div className='space-x-2'>
+                        <button className='bg-[#FCC215] text-[#0E0524] font-bold text-s h-[39px] px-6 py-0 rounded-full mt-2'>
+                            I want to work
+                        </button>
+                        <button className='bg-[#FCC215] text-[#0E0524] font-bold text-s h-[39px] px-6 py-0 rounded-full mt-2'>
+                            I want to hire
+                        </button>
                     </div>
                 </div>
             </div>
-            <div className='w-full md:w-1/2 flex justify-center items-center'>
-                <img src={heroImg}/>
+
+            {/* Right Section */}
+            <div className='flex justify-center items-center'>
+                <img
+                    src={heroImg}
+                    alt='Hero'
+                    className='max-w-full h-auto md:w-[100%]'
+                />
             </div>
-
         </div>
-
     );
 };
 
 export default HeroSection;
-
-
