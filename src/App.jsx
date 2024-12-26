d06905bfd8b1d36ee389e37d1d68cbfcd685200c
D.T.Thennakoon <154307023+dinithiZ@users.noreply.github.com>
2024-12-26 16:53:22 +0530

    update footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import ServiceProvider from './pages/ServiceProvider'; // Ensure this file exists
 
 function App() {
   return (
-    <div className="flex flex-col justify-center">
-      
-      <Home/>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-center">
+       
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ServiceProvider" element={<ServiceProvider />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
